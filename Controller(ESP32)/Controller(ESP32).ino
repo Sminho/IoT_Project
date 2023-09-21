d29a6c6fbcdd7992ed1d6874e56ff83bf4ce2215
신민호 <think890@hotmail.com>
2023-09-21 22:14:07 +0900

    Update Controller(ESP32).ino

--- Controller(ESP32)/Controller(ESP32).ino
+++ Controller(ESP32)/Controller(ESP32).ino
@@ -50,7 +50,7 @@ uint8_t txValue = 0;
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // 필요한 장치에 맞게 UID 생성해서 사용!!
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
